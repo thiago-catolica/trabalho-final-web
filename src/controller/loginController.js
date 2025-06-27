0ae831afd5722d06797c604269ab1b0ab8c9ae39
thiago-catolica <thiago.castro@a.ucb.br>
2025-06-27 11:49:44 -0300

    Atualização Controllers

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,3 +1,5 @@
+const { usuario } = require ('../models/indexModel');
+
 function getTelaLogin(req, res){
     res.render ('tela_login.html');
 }
@@ -6,8 +8,66 @@ function getRecuperarSenha(req, res){
     res.render('tela_recuperacao_senha.html');
 }
 
-function verificarLogin(req, res){
+async function verificarLogin(req, res){
+    
+    const tipo_usuario = await usuario.findOne({
+        where : {
+            email : req.body.email,
+            senha : req.body.senha
+        },
+        attributes : ['tipo_usuario']
+        });
 
+
+    if (tipo_usuario != null){
+        console.log("Usuário Autenticado.");
+
+        const tipoUsuario = tipo_usuario.tipo_usuario;
+
+        if (tipoUsuario == 'Administrador'){
+            res.redirect ('/administrador');
+        }
+        else if (tipoUsuario == 'Aluno'){
+            res.redirect ('/aluno');
+        }
+        else if (tipoUsuario == 'Professor'){
+            res.redirect ('/professor');
+        }
+    }
+    else {
+        let erro = true;
+        res.render('tela_login.html', {erro});
+    }
+
+
+}
+
+async function recuperar_senha (req, res){
+    const Usuario = await usuario.findOne({
+        where : {
+            email : req.body.email,
+            senha : req.body.senhaAntiga
+        }
+    });
+
+    const nova_senha = req.body.senhaNova;
+
+    if (Usuario == null){
+        res.render('tela_recuperacao_senha.html', {erro : true});
+        return;
+    }
+
+    const update_usuario = await usuario.update(
+        {senha : nova_senha},
+        {where : {email : req.body.email}}
+    );
+
+    if (update_usuario[0] >=1){
+        res.render ('tela_recuperacao_senha.html', {sucesso : true});
+    }
+    else {
+        res.render ('tela_recuperacao_senha.html', {erro_banco : true});
+    }
 }
 
 function logout(req,res){
@@ -19,6 +79,7 @@ function logout(req,res){
 module.exports = {
     getTelaLogin, 
     getRecuperarSenha,
+    recuperar_senha,
     verificarLogin,
     logout
 };  
